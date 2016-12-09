50ecfb7ef52fb973a1cbf36d164e0a05a16f1fd3
primo2538 <yo.ratchapon_st@tni.ac.th>
2016-12-09 03:12:09 +0000

    First commit

---
+++ hello-cpp-world.cc
@@ -0,0 +1,19 @@
+
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+
+using namespace std;
+ 
+int main() {
+    
+    srand(time(NULL));
+    
+    for(int i=0;i<100;i++){
+        
+         cout <<  rand() % 100 + 1 << endl;  //number between 1 and 10
+    }
+ 
+     return 0;
+}
+
